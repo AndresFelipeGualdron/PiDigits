0eaf61f1fc2ee2ad0c7e791745f77fcbc4578b3f
AndresFelipeGualdron <andres.gualdron@labinfo.is.escuelaing.edu.co>
2020-01-23 07:43:08 -0500

    Parte 1 terminada

--- src/main/java/edu/eci/arsw/threads/CountThread.java
+++ src/main/java/edu/eci/arsw/threads/CountThread.java
@@ -9,6 +9,25 @@ package edu.eci.arsw.threads;
  *
  * @author hcadavid
  */
-public class CountThread {
-    
+public class CountThread implements Runnable{
+
+    int A;
+    int B;
+
+    public CountThread(int A, int B){
+        this.A = A;
+        this.B = B;
+    }
+
+
+    public void start(){
+        for(int i = A; i < B; i++){
+            System.out.println(i);
+        }
+    }
+
+    @Override
+    public void run() {
+        this.start();
+    }
 }
--- src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
+++ src/main/java/edu/eci/arsw/threads/CountThreadsMain.java
@@ -10,9 +10,23 @@ package edu.eci.arsw.threads;
  * @author hcadavid
  */
 public class CountThreadsMain {
-    
+
+
+
+
+
     public static void main(String a[]){
-        
+        CountThread countThreadOne = new CountThread(0,99);
+        CountThread countThreadTwo = new CountThread(100, 199);
+        CountThread countThreadThree = new CountThread(200,299);
+
+        Thread threadOne = new Thread(countThreadOne);
+        Thread threadTwo = new Thread(countThreadTwo);
+        Thread threadThree = new Thread(countThreadThree);
+
+        threadOne.start();
+        threadTwo.start();
+        threadThree.start();
     }
-    
+
 }
